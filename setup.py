09e6b5fcfa272f3b0c91d9f88e0a1d19c931a522
Phu Nguyen <phu.nguyenduc@protonmail.com>
2020-03-25 13:53:28 +0700

    - Update.

--- setup.py
+++ setup.py
@@ -1,22 +1,5 @@
-import turtle
-import math
-bob = turtle.Turtle()
-bob.delay = 1.0
-print(bob)
+from modules import tkinter
 
-def square(t, length):
-	for i in range(4):
-		t.fd(length)
-		t.lt(45)
-
-def polygon(t, length, n):
-	for i in range(n):
-		t.fd(length)
-		t.lt(360/n)
-
-polygon(bob, 100, 10)
-
-turtle.mainloop()
-
-# fixed 
-ádfádfádfádfádf
+appRoot = tkinter.Tk(screenName="mainScreen")
+appRoot.title('Socketor')
+appRoot.mainloop()
