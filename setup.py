5b321eb020bd2e7fde7ea54fd98edaf963723d93
hoangnhan2405 <62413727+hoangnhan2405@users.noreply.github.com>
2020-03-30 20:30:37 +0700

    test_Temper

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
-import turtle
-import math
-bob = turtle.Turtle()
-bob.delay = 1.0
-print(bob)
-
-def square(t, length):
-	for i in range(4):
-		t.fd(length)
-		t.lt(45)
-
-def polygon(t, length, n):
-	for i in range(n):
-		t.fd(length)
-		t.lt(360/n)
-
-polygon(bob, 100, 10)
-
-turtle.mainloop()
-
-# fixed 
-ádfádfádfádfádf
+from tkinter import *
+def calculate():
+	temp = int(entry.get())
+	temp = 9/5*temp+32
+	output_label.configure(text = 'Converted: {:.1f}'.format(temp))
+	entry.delete(0,END)
+root = Tk()
+message_label = Label(text='Enter a temperature',
+font=('Verdana', 16))
+output_label = Label(font=('Verdana', 16))
+entry = Entry(font=('Verdana', 16), width=4)
+calc_button = Button(text='Ok', font=('Verdana', 16),
+command=calculate)
+message_label.grid(row=0, column=0)
+entry.grid(row=0, column=1)
+calc_button.grid(row=0, column=2)
+output_label.grid(row=1, column=0, columnspan=3)
+mainloop()
