b13ebb4f0886250130eb49a564a241449952b51e
Phu Nguyen <phu.nguyenduc@protonmail.com>
2020-04-20 10:16:29 +0700

    -

--- get-mail.py
+++ get-mail.py
@@ -1,56 +1,18 @@
-from email.parser import HeaderParser
-import smtplib
-import time
-import imaplib
-import email
-
-ORG_EMAIL = "@gmail.com"
-FROM_EMAIL = "nguyen.dphux" + ORG_EMAIL
-SMTP_SERVER = "imap.gmail.com"
-SMTP_PORT = 993
-FROM_PWD = "Viendanbac"
-
-
-def read_email_from_gmail():
-    try:
-        mail = imaplib.IMAP4_SSL(SMTP_SERVER)
-        mail.login(FROM_EMAIL, FROM_PWD)
-        mail.select('inbox')
-
-        type, data = mail.search(None, 'ALL')
-        mail_ids = data[0]
-        print(data)
-
-        id_list = mail_ids.split()
-        first_email_id = int(id_list[0])
-        latest_email_id = int(id_list[-1])
-        for i in range(latest_email_id, first_email_id, -1):
-            typ, data = mail.fetch(i, '(RFC822)')
-
-            for response_part in data:
-                if isinstance(response_part, tuple):
-                    msg = email.message_from_string(response_part[1])
-                    email_subject = msg['subject']
-                    email_from = msg['from']
-                    print('From : ' + email_from + '\n')
-                    print('Subject : ' + email_subject + '\n')
-
-    except Exception as e:
-        print(str(e))
-
-read_email_from_gmail()
-
-
 import email
 import imaplib
+import poplib
+import string
+import random
+from io import StringIO
+import logging
 
-EMAIL = 'mygmail@mail.com'
-PASSWORD = 'pass'
 SERVER = 'imap.gmail.com'
 
 # connect to the server and go to its inbox
 mail = imaplib.IMAP4_SSL(SERVER)
+print(34)
 mail.login(EMAIL, PASSWORD)
+print(23)
 # we choose the inbox but you can select others
 mail.select('inbox')
 
