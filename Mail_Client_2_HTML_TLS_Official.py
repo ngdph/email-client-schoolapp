5c0f96f643a021f506a7259db0d670633121dad9
hoangnhan2405 <62413727+hoangnhan2405@users.noreply.github.com>
2020-04-28 12:38:05 +0700

    Update Mail_Client_2_HTML_TLS_Official.py

--- Mail_Client_2_HTML_TLS_Official.py
+++ Mail_Client_2_HTML_TLS_Official.py
@@ -1,4 +1,7 @@
-import smtplib
+import smtplib, email
+
+from email import encoders
+from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
@@ -7,55 +10,72 @@ smtp_server = "smtp.gmail.com"
 
 sender_email = "nhanth240500@gmail.com"
 receiver_email = "18520326@gm.uit.edu.vn"
-password = input("Type your password and press enter: ")
+password = "@177687Nhan@"
 
-def Send_Mail():
-	msg = MIMEMultipart(); 
+msg = MIMEMultipart()
 
-	#msg = MIMEMultipart("alternative"); #Dùng khi gửi theo dạng html
+#msg = MIMEMultipart("alternative"); #Dùng khi gửi theo dạng html
 
-	#Thông tin về From, To, Subject, Bcc của mail.
-	msg["From"] = sender_email
-	msg["To"] = receiver_email
-	msg["Subject"] = "Gui Nhan."
-	msg["Bcc"] = receiver_email
+#Thông tin về From, To, Subject, Bcc của mail.
+msg["From"] = sender_email
+msg["To"] = receiver_email
+msg["Subject"] = "Gui Nhan."
+msg["Bcc"] = receiver_email
 
 	#Message của người gửi muốn người nhận nhận được
-	text = """\
-	Hello Nhan,
-	Dao nay m khoe khong?
-	Facebook cua tao ne
-	Hen m toi nay 8h nhe!
-	Good bye."""
+body_mail = """\
+Hello Nhan,
+Dao nay m khoe khong?
+Facebook cua tao ne
+Hen m toi nay 8h nhe!
+Good bye."""
 
-	'''html = """\
-	<html>
-		<body>
-			<p>
-				Hello Nhan,<br>
-				Dao nay m khoe khong?<br>
-				<a href = "https://www.facebook.com/hoanggnhann/"> Facebook cua tao ne </a>
-			</p>
-		</body>
-	</html>
-	"""'''
-
-	#Định dạng message của mail theo kiểu plain text và lưu vào part1
-	part1 = MIMEText(text, "plain") 
+	#Định dạng message của mail theo kiểu plain text và lưu vào message_mail
+message_mail = MIMEText(body_mail, "plain") 
 	#part2 = MIMEText(html, "html")
 
 	#Đính kèm nội dung mail đang được lưu trong par1 vào msg
-	msg.attach(part1)
+msg.attach(message_mail)
 	#msg.attach(part2)
 
-	try:
-		#Dùng smtp để kết nối với smtp server theo giao thức TLS
-		#Trong 
-		server = smtplib.SMTP(smtp_server, port)
-		server.starttls()
-		server.login(sender_email, password)
-		server.sendmail(sender_email, receiver_email, msg.as_string())
-	except Exception as e:
-		print(e)
-	finally:
-		server.quit()
+filename = "listmails.txt"  # In same directory as script
+
+	# Open PDF file in binary mode
+with open(filename, "rb") as attachment:
+		# Add file as application/octet-stream
+		# Email client can usually download this automatically as attachment
+	file_mail = MIMEBase("application", "octet-stream")
+	file_mail.set_payload(attachment.read())
+
+	# Encode file in ASCII characters to send by email    
+encoders.encode_base64(file_mail)
+
+	# Add header as key/value pair to attachment part
+file_mail.add_header(
+	"Content-Disposition",
+	f"attachment; filename= {filename}",
+)
+
+msg.attach(file_mail)
+all_message = msg.as_string()
+
+try:
+		#Tạo một đối tượng SMTP, cho phép kết nối tới server của SMTP và cho phép sử dụng các phương thức của SMTP
+	server = smtplib.SMTP(smtp_server, port)
+
+		#Tạo kết nối SMTP không bảo mật và mã hóa nó với starttls()
+	server.starttls()
+
+		#Đăng nhập tài khoản gmail của người gửi
+	server.login(sender_email, password)
+
+		#Tiến hành gửi mail từ người gửi tới người nhận, message được định dang theo string.
+	server.sendmail(sender_email, receiver_email, all_message)
+	
+	#Trong trường hợp có lỗi khi kết nối tới server của SMTP hoặc xảy ra bất kì lỗi gì trong quá trình xử lí
+	#Sẽ xuất thông báo lỗi
+except Exception as e:
+	print(e)
+
+finally:
+	server.quit()
