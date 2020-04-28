8c7c78ce1a90deecffb80cf8864ac31bd02dc344
yuHNguyen <62386960+yuHNguyen@users.noreply.github.com>
2020-04-28 11:04:56 +0700

    Update GUI

--- sendmail.py
+++ sendmail.py
@@ -1,5 +1,14 @@
 from tkinter import*
 
+def SlcBack():
+    SendMail.destroy()
+    import option
+
+
 SendMail = Tk()
 SendMail.title("Gửi Mail")
 SendMail.geometry("300x300")
+
+### button để quay lại tab option
+btnBack = Button(SendMail,text="Back",command = SlcBack)
+btnBack.place(x=250,y=260)
