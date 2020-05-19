b6d38d62671833861e2c1d77faa32d3737596181
hoangnhan2405 <62413727+hoangnhan2405@users.noreply.github.com>
2020-05-19 09:23:48 +0700

    Create RSA.py

---
+++ RSA.py
@@ -0,0 +1,30 @@
+from Crypto.PublicKey import RSA
+from Crypto import Random
+from Crypto.Cipher import PKCS1_OAEP
+
+
+def rsa_encrypt(plaintext, key):
+    public_key = key.publickey().exportKey("PEM")
+    plaintext = str.encode(plaintext)
+
+    rsa_public_key = RSA.importKey(public_key)
+    rsa_public_key = PKCS1_OAEP.new(rsa_public_key)
+
+    encrypted_text = rsa_public_key.encrypt(plaintext)
+    return encrypted_text
+
+
+def rsa_decrypt(ciphertext, key):
+    private_key = key.export_key("PEM")
+    rsa_private_key = RSA.importKey(private_key)
+    rsa_private_key = PKCS1_OAEP.new(rsa_private_key)
+    decrypted_text = rsa_private_key.decrypt(ciphertext)
+    return decrypted_text
+
+
+plaintext = "Tien ngu lon"
+key = RSA.generate(2048)
+ciphertext = rsa_encrypt(plaintext, key)
+print("ciphertext ", ciphertext)
+plain = rsa_decrypt(ciphertext, key)
+print("plain ", plain)
